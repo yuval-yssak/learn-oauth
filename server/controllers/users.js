9fd2c3804cefb288578456f2f4e597852b067f9b
iswara108 <iswara@sivananda.org>
2020-05-03 20:00:03 -0400

    added some dao functions

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,5 +1,23 @@
+import { init, createUser, findUser } from '../dao/users.js'
+
 async function signUp(req, res, next) {
   // supposed to receive email and password
+
+  const { email, password } = req.value.body
+
+  // Check if there is a user with the same email
+  const foundUser = await findUser({ email })
+  console.log('founduser is', email, foundUser)
+  if (foundUser) {
+    return res.status(403).json({ error: 'Email is already in use' })
+  }
+
+  // Create a new user
+  const createResult = await createUser(email, password)
+  console.log('created', createResult.result)
+
+  // Respond with token
+  res.json({ user: 'created' })
 
   res.status(200).json({ result: 'success' })
 }
--- server/tests/server/routes/users.test.js
+++ server/tests/server/routes/users.test.js
@@ -1,19 +1,19 @@
-// will work for linux for windows we are going to user cross-env in package json
-//process.env.NODE_ENV = 'test';
-
 import chai from 'chai'
 import chaiHttp from 'chai-http'
 import faker from 'faker'
-// import mongoose from 'mongoose'
+
 const { expect } = chai
 
 import server from '../../../app.js'
+import { init, destroy } from '../../../dao/users.js'
+import { connect, disconnect } from '../../../mongoClient.js'
 
 chai.use(chaiHttp)
 
 let token
 
-describe('Users route', () => {
+describe('Users route', function () {
+  let DBclient
   const signup = '/auth/signup'
   const signin = '/auth/signin'
   const secret = '/auth/secret'
@@ -26,40 +26,33 @@ describe('Users route', () => {
     password: faker.internet.password()
   }
 
-  before(done => {
-    chai
-      .request(server)
-      .post(signup)
-      .send(preSave)
-      .end((err, res) => {
-        expect(res.status).to.equal(200)
-        token = res.body.token
-        done()
-      })
+  before(async function () {
+    try {
+      DBclient = await connect()
+      await init(DBclient)
+      const res = await chai.request(server).post(signup).send(preSave)
+      expect(res.status).to.equal(200)
+      token = res.body.token
+    } catch (e) {
+      console.error(e)
+      throw e
+    }
   })
 
   // after all test have run we drop our test database
-  after('droping test db', () => {
-    // mongoose.connection.dropDatabase(() => {
-    //   console.log('\n Test database dropped')
-    // })
-    // mongoose.connection.close(() => {
-    //   done()
-    // })
+  after('droping test db', async function () {
+    console.log('about to destroy')
+    await destroy()
+    await disconnect(DBclient)
   })
 
   describe('signup', () => {
-    it('should crete new user if email not found', done => {
-      chai
-        .request(server)
-        .post(signup)
-        .send(user)
-        .end((err, res) => {
-          expect(res.status).to.equal(200)
-          expect(res.body).not.to.be.empty
-          expect(res.body).to.have.property('token')
-          done()
-        })
+    it('should crete new user if email not found', async function () {
+      const res = await chai.request(server).post(signup).send(user)
+
+      expect(res.status).to.equal(200)
+      expect(res.body).not.to.be.empty
+      expect(res.body).to.have.property('token')
     })
 
     it('should return 403 if email was found', done => {
@@ -77,55 +70,54 @@ describe('Users route', () => {
     })
   })
 
-  describe('secrete', () => {
-    it('should return status 401', done => {
-      chai
-        .request(server)
-        .get(secret)
-        .end((err, res) => {
-          expect(res.status).to.equal(401)
-          expect(res.body).to.be.empty
-          done()
-        })
-    })
+  // describe('secrete', () => {
+  // it('should return status 401', done => {
+  //   chai
+  //     .request(server)
+  //     .get(secret)
+  //     .end((err, res) => {
+  //       expect(res.status).to.equal(401)
+  //       expect(res.body).to.be.empty
+  //       done()
+  //     })
+  // })
+  // it('should return status 200', done => {
+  //   chai
+  //     .request(server)
+  //     .get(secret)
+  //     .set('Authorization', token)
+  //     .end((err, res) => {
+  //       expect(res.status).to.equal(200)
+  //       expect(res.body).to.deep.equal({ secret: 'resource' })
+  //       done()
+  //     })
+  // })
+  // })
 
-    it('should return status 200', done => {
-      chai
-        .request(server)
-        .get(secret)
-        .set('Authorization', token)
-        .end((err, res) => {
-          expect(res.status).to.equal(200)
-          expect(res.body).to.deep.equal({ secret: 'resource' })
-          done()
-        })
-    })
-  })
+  // describe('signin', () => {
+  //   it('should return error 400 if user email and password empty', done => {
+  //     let user = {}
+  //     chai
+  //       .request(server)
+  //       .post(signin)
+  //       .send(user)
+  //       .end((err, res) => {
+  //         expect(res.status).to.be.equal(400)
+  //         done()
+  //       })
+  //   })
 
-  describe('signin', () => {
-    it('should return error 400 if user email and password empty', done => {
-      let user = {}
-      chai
-        .request(server)
-        .post(signin)
-        .send(user)
-        .end((err, res) => {
-          expect(res.status).to.be.equal(400)
-          done()
-        })
-    })
-
-    it('should return 200 and our token', done => {
-      chai
-        .request(server)
-        .post(signin)
-        .send(preSave)
-        .end((err, res) => {
-          expect(res.status).to.be.equal(200)
-          expect(res.body).not.to.be.empty
-          expect(res.body).to.have.property('token')
-          done()
-        })
-    })
-  })
+  //   it('should return 200 and our token', done => {
+  //     chai
+  //       .request(server)
+  //       .post(signin)
+  //       .send(preSave)
+  //       .end((err, res) => {
+  //         expect(res.status).to.be.equal(200)
+  //         expect(res.body).not.to.be.empty
+  //         expect(res.body).to.have.property('token')
+  //         done()
+  //       })
+  //   })
+  // })
 })
