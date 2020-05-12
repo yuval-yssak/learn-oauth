0f6842b7f7c6eeb728f51ac980b9867c552b784a
iswara108 <iswara@sivananda.org>
2020-05-12 14:47:54 -0400

    proof of concept for react-side oauth

--- client/src/components/Signin.js
+++ client/src/components/Signin.js
@@ -1,3 +1,20 @@
 import React from 'react'
+import { GoogleLogin } from 'react-google-login'
 
-export default () => <h3>Welcome! This is our sign in page.</h3>
+const responseGoogle = response => {
+  console.log(response)
+}
+
+function SigninGoogle() {
+  return (
+    <GoogleLogin
+      clientId="1053321495897-q71eb83fh2c6t37dql08valh2k3imemo.apps.googleusercontent.com"
+      buttonText="Login"
+      onSuccess={responseGoogle}
+      onFailure={responseGoogle}
+      cookiePolicy={'single_host_origin'}
+    />
+  )
+}
+
+export default SigninGoogle
